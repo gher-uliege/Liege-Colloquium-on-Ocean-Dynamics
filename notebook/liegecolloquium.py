3f58784d41d95ff7d1fa56ca7898a0fd7b228d26
ctroupin <charles.troupin@gmail.com>
2017-07-14 21:06:28 +0200

    corrected list comparison

--- notebook/liegecolloquium.py
+++ notebook/liegecolloquium.py
@@ -1,10 +1,24 @@
 import os
+import sys
 import glob
 import logging
 from collections import Counter
-from geopy.geocoders import Nominatim
-geolocator = Nominatim()
+from geopy.geocoders import Nominatim, ArcGIS, GoogleV3, OpenMapQuest
 
+
+arcgis = ArcGIS(timeout=100)
+nominatim = Nominatim(timeout=100)
+googlev3 = GoogleV3(timeout=100)
+openmapquest = OpenMapQuest(timeout=100)
+
+logloc= logging.getLogger('locator')
+logloc.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+logloc.addHandler(ch)
+
+# choose and order your preference for geocoders here
+geocoders = [nominatim, googlev3, arcgis, openmapquest]
 
 class Participant(object):
    
@@ -18,7 +32,9 @@ class Participant(object):
         self.country = country
     
     def __repr__(self):
-        return str(self.__dict__)
+        return "Participant {0} {1} ({2})".format(self.firstname,
+                                                     self.name,
+                                                     self.country)
         
     def replace_country(self):
         dictcountry = {"U.S.A.": "United States of America", 
@@ -27,15 +43,59 @@ class Participant(object):
             self.country = self.country.replace(k, v)
             
     def get_location(self):
-        # Try with different combinations of affil., city, country
+        # Try with different combinations of affiliation, city, country
+
         locationstring = ", ".join((self.affiliation, self.city, self.country))
-        location = geolocator.geocode(locationstring)
-        if not(location):
-            locationstring = ", ".join((self.city, self.country))
-            location = geolocator.geocode(locationstring)
-            
-        self.location = location
+        logloc.info(locationstring)
+
+        i = 0
+        lon = None
+        lat = None
+
+        while not(lon) and not (lat):
+
+            # logloc.debug("i = {0}".format(i))
+            try:
+                # try to geocode using a service
+                location = geocoders[i].geocode(locationstring)
+
+                # if it returns a location
+                if location != None:
+                    logloc.debug("Ok with Geocoder: {0}".format(geocoders[i]))
+                    # return those values
+                    lat = location.latitude
+                    lon = location.longitude
+                else:
+                    logloc.debug("Geocoder {0} did not provide coordinates".format(geocoders[i]))
+
+            except:
+                # catch whatever errors, likely timeout, and return null values
+                print("Problem with geocoder {0}".format(geocoders[i]))
+
+            # Try another geocoder
+            i += 1
+
+        self.lon = lon
+        self.lat = lat
+
         return locationstring
+
+    def affiliation_exist(self, participant_list):
+        """
+        Check if 2 instances of Participant have the same affilation
+        :param participant_list: list of participants
+        """
+        if self.affiliation == other.affiliation:
+            if self.city == self.city:
+                if self.country == self.country:
+                    return True
+                else:
+                    return False
+            else:
+                return False
+        else:
+            return False
+
     
     def write_to(self, filename, sep='\t'):
         """
@@ -44,8 +104,8 @@ class Participant(object):
         with open(filename, 'a') as f:
             f.write(sep.join((self.name, self.firstname, 
                              self.affiliation, self.country,
-                             str(self.location.latitude),
-                             str(self.location.longitude), "\n"))
+                             str(self.lat),
+                             str(self.lon), "\n"))
                    )
     
     def write_coords_to(self, filename, sep="\t"):
@@ -53,5 +113,4 @@ class Participant(object):
         Write the coordinates in a file
         """
         with open(filename, 'a') as f:
-            f.write("[{0}, {1}],\n".format(self.location.latitude,
-                                               self.location.longitude)) 
+            f.write("[{0}, {1}],\n".format(self.lat, self.lon))
